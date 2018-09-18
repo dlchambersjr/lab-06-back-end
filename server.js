a41aac7f282ddedbf1a2c5090c8c3c444943cc86
dlchambersjr <dlchambersjr@gmail.com>
2018-09-18 11:31:58 -0700

    request paramenter renamed

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ require('dotenv').config();
 
 const PORT = 3000;
 
-app.get('/location', (requestAnimationFrame, response) => {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GOOGLE_API_KEY}`;  
+app.get('/location', (request, response) => {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GOOGLE_API_KEY}`;
 
   return superagent.get(url)
-  .then(result => {
-    const locationResult = {
-      search_query: request.query.data,
-      formatted_query: result.body.results[0].formatted_address,
-      latitude: result.body.results[0].geometry.location.lat,
-      longitude: result.body.resultsp[0].geometry.location.lng
-    }
-    response.send(locationResult);
-  })
-}) 
+    .then(result => {
+      const locationResult = {
+        search_query: request.query.data,
+        formatted_query: result.body.results[0].formatted_address,
+        latitude: result.body.results[0].geometry.location.lat,
+        longitude: result.body.resultsp[0].geometry.location.lng
+      }
+      response.send(locationResult);
+    })
+})
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
